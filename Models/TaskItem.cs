796b03902ff308b46bad0707b33ab64e12a87ca1
GUSTAVO DA COSTA PAULINO <gustavo2000926@creasp.org.br>
2025-06-23 15:01:03 -0300

    Configuração inicial com EF Core e estrutura MVC

---
+++ Models/TaskItem.cs
@@ -0,0 +1,8 @@
+namespace TaskManagerWeb.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; } = false;
+}
